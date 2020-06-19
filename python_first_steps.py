1511a768d0496e58fb8953d743b0001f1ac2e8a0
ecardena19 <elcardenas09@gmail.com>
2020-06-19 19:56:56 -0400

    Practice
    Practicing python code types and other

---
+++ python_first_steps.py
@@ -0,0 +1,43 @@
+
+num1 = 10
+num2 = 1.5
+num3 = num1 + num2
+print(num3)
+
+sub1 = 3 - 5
+print(sub1)
+
+div1 = float(10 / 3)
+print(div1)
+
+num4 = int(3 - 5)
+
+float1 = 15.0.is_integer()
+print(float1)
+
+message = 'happy birthday'
+message2 = 'Emilio'
+print(message + " " + message2)
+print(message[0:5])
+
+list1 = [3, 5, 6, 11]
+list1.pop(0)
+print(list1)
+list1.append(10)
+print(list1)
+
+name1 = 'Jim'
+name2 = 'James'
+greeting = 'my name is {}'.format(name1)
+greeting2 = 'my name is {}'.format(name2)
+print(greeting)
+print(greeting2)
+
+baseball = ['Yankees', 'Astros', 'Marlins']
+print(baseball[-2])
+player_stats = {'Batting Average': '.340', 'Slugging': '1.028', 'RBI': '500'}
+print(player_stats['Slugging'])
+player_stats.update({'Total Bases': '540'})
+print(player_stats)
+Result1 = player_stats.get('Slugging')
+
