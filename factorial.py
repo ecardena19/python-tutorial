4fa3863980d6682d6745b384abcf06a654e38476
ecardena19 <elcardenas09@gmail.com>
2020-06-24 23:15:24 -0400

    Calculate Factorials
    Utilizing "for" loop to calculate factorials

--- factorial.py
+++ factorial.py
@@ -1,8 +1,13 @@
 # For example, 5! = 5 * 4 * 3 * 2 * 1 = 120
-N = 5
+N = 10
 fac_value = 1
 fac5 = [5, 4, 3, 2, 1]
 for fac_res in fac5:
     fac_value = fac_value * fac_res
-
 print(fac_value)
+
+fac_value2 = 1
+for fac10 in range(1, 11, 1):
+    fac_value2 = fac_value2 * fac10
+print(fac_value2)
+
