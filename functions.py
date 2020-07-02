710bf9bf3bf6d380e6ae524ccd2334af3f76675b
ecardena19 <elcardenas09@gmail.com>
2020-07-01 23:19:06 -0400

    Function Practice

--- functions.py
+++ functions.py
@@ -2,8 +2,64 @@ def hello_world():
     return 'hello world'
 
 
-result = hello_world()
+for x in range(3):  # [0, 1, 2]
+    print(hello_world())
 
-message = '{} {} {}'.format(result, result, result)
 
-print(message)
+def print_hello_world():
+    print('hello world')
+
+
+for x in range(3):
+    print_hello_world()
+
+
+def print_my_name(name):
+    print('hello ' + name)
+
+
+print_my_name('emilio')
+
+
+# [print_my_name(n) for n in ['emilio', 'adam']]
+# map(print_my_name, ['emilio', 'adam'])
+
+
+def addition(int1, int2):
+    return int1 + int2
+
+
+print(addition(1, 2))
+
+
+def multiply(int3, int4):
+    return int3 * int4
+
+
+print(multiply(3, 100))
+
+
+def division(float1, float2):
+    return int(float1 / float2)
+
+
+print(division(25, 12.2))
+
+# function composition
+
+print(multiply(10, addition(1, 3)))
+print(10 * 1 + 3)
+
+
+def to_lower_case(string):
+    return string.lower()
+
+
+def to_upper_case(string):
+    return string.upper()
+
+
+print(to_lower_case(to_upper_case('test')))
+print(to_upper_case(to_lower_case('test')))
+
+print(multiply(5, division(5, addition(5, 1))))
