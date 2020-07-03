a0e5fbbf219c6b50ac2174ce0158bb274c960d46
ecardena19 <elcardenas09@gmail.com>
2020-07-02 21:29:31 -0400

    Operators Practice

---
+++ hacker_rank/arithmetic_operators_hr.py
@@ -0,0 +1,24 @@
+num1 = int(input())
+num2 = int(input())
+
+
+def addition(int1, int2):
+    return int1 + int2
+
+
+print(addition(num1, num2))
+
+
+def subtraction(int1, int2):
+    return int1 - int2
+
+
+print(subtraction(num1, num2))
+
+
+def multiply(int1, int2):
+    return int1 * int2
+
+
+print(multiply(num1, num2))
+
