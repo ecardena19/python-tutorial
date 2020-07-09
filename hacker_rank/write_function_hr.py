4996330cd7008229e535873fcd148d1df8f532ce
ecardena19 <elcardenas09@gmail.com>
2020-07-08 22:56:21 -0400

    Leap Year - Nested if function

--- hacker_rank/write_function_hr.py
+++ hacker_rank/write_function_hr.py
@@ -1,13 +1,12 @@
-n = int(input())
-
-result1 = 'True'
-result2 = 'False'
-
-if n % 4 == 0 and n % 400 == 0:
-    print(result1)
-
-else:
-    print(result2)
+def is_leap(n):
+    if n % 4 == 0:
+        if n % 100 == 0:
+            if n % 400 == 0:
+                return True
+            return False
+        return True
+    return False
 
 
-
+year = int(input())
+print(is_leap(year))
