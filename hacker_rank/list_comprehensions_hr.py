aa58f3c3191200b6dc82ce1ad5a6627ec1a43e06
ecardena19 <elcardenas09@gmail.com>
2020-07-08 22:55:04 -0400

    Generate Coordinates

--- hacker_rank/list_comprehensions_hr.py
+++ hacker_rank/list_comprehensions_hr.py
@@ -3,7 +3,18 @@ y = int(input())
 z = int(input())
 n = int(input())
 
-coordinates = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if ((i + j + k) != n)]
+# coordinates = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if ((i + j + k) != n)]
+
+coordinates = []
+for i in range(x + 1):
+    for j in range(y + 1):
+        for k in range(z + 1):
+            # i=0 j=0 k=0
+            # i=0 j=0 k=1
+            # ...
+            # i=0 j=1 k=0
+            if (i + j + k) != n:
+                coordinates.append([i, j, k])
 
 print(coordinates)
 
