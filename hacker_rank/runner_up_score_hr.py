9da09ecceece03ba26216901090dc54ec518d7fb
ecardena19 <elcardenas09@gmail.com>
2020-07-08 22:55:57 -0400

    Runner Up Score - map function

--- hacker_rank/runner_up_score_hr.py
+++ hacker_rank/runner_up_score_hr.py
@@ -1,7 +1,12 @@
 num_tests = int(input())
+test_str = input().strip().split()
 
-test_scores = list(map(int, input().strip().split()))
+# [int('3'), int('2'), int('22')]
+test_scores = list(map(int, test_str))
 
 second_max = set(test_scores)  # removes duplicates in list
 second_max.remove(max(second_max))  # removes max within set
 print(max(second_max))
+
+
+# print(a[a.count(a[0])])
