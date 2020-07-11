9209d09c6c33eb755a96bb33d171ec5014a68c88
ecardena19 <elcardenas09@gmail.com>
2020-07-11 16:46:58 -0400

    Lowest Exam Score

--- hacker_rank/nested_lists_hr.py
+++ hacker_rank/nested_lists_hr.py
@@ -1,4 +1,20 @@
-num_students = int(input())
+# num_students = int(input())
 
-student_names = list(map(str, input()))
-test_scores = list(map(int, input()))
+# student_names = list(map(str, input()))
+# test_scores = list(map(int, input()))
+
+# student_names = str(input())
+# test_scores = float(input())
+
+test_records = []
+
+n = int(input())  # number of students in class
+
+for i in range(n):
+    names_scores = [str(input()), float(input())]  # creates nested list with two different data types
+    test_records.append(names_scores)
+
+low_scores = sorted(test_records[1])
+
+print(low_scores)
+
