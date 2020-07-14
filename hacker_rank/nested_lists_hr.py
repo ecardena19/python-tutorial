168ea2157805d7d7648dc786a2107639e0c42941
ecardena19 <elcardenas09@gmail.com>
2020-07-13 22:57:19 -0400

    Second Lowest Nested Lists

--- hacker_rank/nested_lists_hr.py
+++ hacker_rank/nested_lists_hr.py
@@ -1,20 +1,30 @@
-# num_students = int(input())
-
-# student_names = list(map(str, input()))
-# test_scores = list(map(int, input()))
-
-# student_names = str(input())
-# test_scores = float(input())
 
 test_records = []
 
 n = int(input())  # number of students in class
 
 for i in range(n):
-    names_scores = [str(input()), float(input())]  # creates nested list with two different data types
-    test_records.append(names_scores)
+    names_scores = [str(input()), float(input())]  # creates format of nested list (string, float)
+    test_records.append(names_scores)  # appends inputs into the nested list
 
-low_scores = sorted(test_records[1])
+test_records.sort(key=lambda x: x[1])  # sorts test scores from lowest to highest
 
-print(low_scores)
+
+lowest_grade = test_records[0][1]
+second_lowest_grade = None
+names = []
+
+for tr in test_records:
+    # tr ['name', grade]
+    if tr[1] > lowest_grade and second_lowest_grade is None:
+        second_lowest_grade = tr[1]
+
+    if second_lowest_grade == tr[1]:
+        names.append(tr[0])
+
+names.sort()
+for name in names:
+    print(name)
+
+
 
