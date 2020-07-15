4317e7fb69d8b9b0a371493f538d013d7d70515a
ecardena19 <elcardenas09@gmail.com>
2020-07-14 21:35:42 -0400

    Loop List Manipulation

--- hacker_rank/list_hr.py
+++ hacker_rank/list_hr.py
@@ -15,6 +15,21 @@ for _ in range(n):
     if 'print' in instruction:
         print(ls)
 
+    if 'remove' in instruction:
+        items2 = instruction.split()
+        index2 = int(items2[1])
+        ls.remove(index2)
 
+    if 'append' in instruction:
+        items3 = instruction.split()
+        index3 = int(items3[1])
+        ls.append(index3)
 
+    if 'sort' in instruction:
+        ls.sort()
 
+    if 'pop' in instruction:
+        ls.pop()
+
+    if 'reverse' in instruction:
+        ls.reverse()
