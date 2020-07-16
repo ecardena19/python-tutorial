f4435e2810c063874e78a70514bdf7c0c426a21a
ecardena19 <elcardenas09@gmail.com>
2020-07-15 21:11:41 -0400

    Incorporated notes

--- hacker_rank/list_hr.py
+++ hacker_rank/list_hr.py
@@ -6,7 +6,7 @@ for _ in range(n):
     instruction = input()
 
     if 'insert' in instruction:
-        # insert 3 11 => [insert, 3, 11]
+        # 'insert 3 11' => ['insert', '3', '11']
         items = instruction.split()
         index = int(items[1])
         obj = int(items[2])
