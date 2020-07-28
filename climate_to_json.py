1a4823fea9c87dc73b04f56341f4bdd64027e516
ecardena19 <elcardenas09@gmail.com>
2020-07-27 23:01:31 -0400

    creating classes

--- climate_to_json.py
+++ climate_to_json.py
@@ -1,5 +1,26 @@
 from climate import hourly
 
+
+class ClimateHour(object):
+    line_no = 0
+    date = None
+    temp = None
+
+    # constructor
+    def __init__(self, line_no, date, temp):
+        self.line_no = line_no
+        self.temp = temp
+        self.date = date
+
+    # override default str implementation
+    def __str__(self):
+        return "Line#: {} Date: {} Temp: {}".format(
+            self.line_no,
+            self.date,
+            self.temp
+        )
+
+
 # convert csv data to a class, then print as JSON
 
 # 4/1/2012 0:00	2012	4	1	0:00		4.3		2.3		87		30		28		12.9		99.88						Rain
@@ -14,4 +35,10 @@ for line in hourly.data():
     if data[6] == '':
         continue
 
-    print(line_count, str(data[0]), float(data[6]))
+    climate_hour = ClimateHour(
+        line_count,
+        str(data[0]),
+        float(data[6])
+    )
+
+    print(climate_hour)
